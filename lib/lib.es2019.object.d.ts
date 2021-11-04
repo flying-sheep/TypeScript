4824a988de56afa2843b5d9bd5817c24f3161157
Philipp A <flying-sheep@web.de>
2021-11-04 09:53:44 +0100

    Stronger typing for keys in fromEntries result

--- lib/lib.es2019.object.d.ts
+++ lib/lib.es2019.object.d.ts
@@ -25,7 +25,7 @@ interface ObjectConstructor {
      * Returns an object created by key-value entries for properties and methods
      * @param entries An iterable object that contains key-value entries for properties and methods.
      */
-    fromEntries<T = any>(entries: Iterable<readonly [PropertyKey, T]>): { [k: string]: T };
+    fromEntries<K extends string, T = any>(entries: Iterable<readonly [K, T]>): { [k in K]: T };
 
     /**
      * Returns an object created by key-value entries for properties and methods
